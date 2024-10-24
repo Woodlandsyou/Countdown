1f9fc038e5a9c85bbc82b56dc5f94eef33068afc
Woodlandsyou <linus_hahle@web.de>
2024-10-24 21:04:41 +0200

    rewrote to clock format

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-const container = document.getElementById('container');
 let count = 10000000;
 
 
 setInterval(() => {
     let time = count;
-    container.children[0].textContent = `${Math.floor(time / 8640000)} d`;
-    time -= Math.floor(time / 8640000) * 8640000;
-    container.children[1].textContent = `${Math.floor(time / 360000)} h`;
-    time -= Math.floor(time / 360000) * 360000;
-    container.children[2].textContent = `${Math.floor(time / 60000)} m`;
-    time -= Math.floor(time / 60000) * 60000;
-    container.children[3].textContent = `${Math.floor(time / 1000)} s`;
-    time -= Math.floor(time / 1000) * 1000;
-    container.children[4].textContent = `${Math.floor(time)} ms`;
+    let h = Math.floor(time / 360000);
+    time -= h * 360000;
+    let m = Math.floor(time / 60000);
+    time -= m * 60000;
+    let s = Math.floor(time / 1000);
+    time -= s * 1000;
+    ms = Math.floor(time);
+    document.getElementById('text').textContent = `${h} : ${m} : ${s} : ${ms}`;
     count--;
 }, 1);
